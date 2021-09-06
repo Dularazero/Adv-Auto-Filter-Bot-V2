f7f57b6d7dcc6d8b840599ccbb2b5c906bb9c467
Dularazero <86337319+Dularazero@users.noreply.github.com>
2021-09-06 14:53:01 +0530

    Update translation.py

--- bot/translation.py
+++ bot/translation.py
@@ -9,8 +9,8 @@ class Translation(object):
 
 Just Add Me To Your Group And Channel And Connect Them And See My Pevers 🔥🔥😝
 
-For More Details Click Help Button Below..
-@CrazyBotsz
+If You want Films Just Come Our Group..
+@whatsappgang
 </i>"""    
     
     HELP_TEXT = """
